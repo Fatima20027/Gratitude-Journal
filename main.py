c8a4698f87926f22b8c5c7ddef3a2ca356732826
Fatima20027 <fatima.hailou@icloud.com>
2024-03-17 17:28:50 +0000

    modifying the code

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 
-print("###WELCOME TO YOUR JOURNAL")
-print("Please choose the things you want to write about.")
+print("               WELCOME TO YOUR JOURNAL!", end="\n\n")
+print("Please choose the things you want to write about, or type 'exit' to exit.(select number)", end="\n\n")
 print("""1. Write about what you are grateful for today.
-         2. Write about how you define happiness.
+2. Write about how you define happiness.
+3. Write about the things you love about yourself.
+4. Write about the things that you don't like about yourself.
+5. write a motivational letter for your future self.
     """)
 
 while True:
     choice = input("<><><>: ")
-    i = 2
+    if choice == 'exit':
+        break
+    elif choice == '5':
+        write = input("Write a motivational letter for your future self:\n")
+        print("Your motivational letter for your future self has been saved.\n")
+        continue
+    elif choice not in ['1', '2', '3', '4']:
+        print("Not valid, try again.\n")
+        continue
+
+    i = 1
     while choice != 'exit':
         if choice == '1':
+            print(f"{i}.", end = "\n")
             write = input(" ")
-            print(f"{i}. {write}", end = "\n")
-            choice = input('')
+        elif choice == '2':
+            print(f"{i}.", end = "\n")
+            write = input(" ")
+        elif choice == '3':
+            print(f"{i}.", end = "\n")
+            write = input(" ")
+        elif choice == '4':
+            print(f"{i}.", end = "\n")
+            write = input(" ")
+     
         i += 1
+       
+         
+    
+    
+        
+
+        
+
+
+
+
+
